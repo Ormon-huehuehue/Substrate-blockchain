aa7778e165418660472cf5e14aae7a064722c6e6
ormon <armnvrma10@gmail.com>
2025-10-25 20:06:02 +0530

    initial commit

---
+++ runtime/src/utxo.rs
@@ -0,0 +1,115 @@
+use super::Aura;
+use codec::{Decode, Encode};
+use frame_support::{
+	decl_event, decl_module, decl_storage,
+	dispatch::{DispatchResult, Vec},
+	ensure,
+};
+use sp_core::{H256, H512};
+#[cfg(feature = "std")]
+use serde::{Deserialize, Serialize};
+use sp_core::sr25519::{Public, Signature};
+use sp_runtime::traits::{BlakeTwo256, Hash, SaturatedConversion};
+use sp_std::collections::btree_map::BTreeMap;
+use sp_runtime::transaction_validity::{TransactionLongevity, ValidTransaction};
+
+pub trait Trait: system::Trait {
+	type Event: From<Event> + Into<<Self as system::Trait>::Event>;
+}
+
+#[cfg_attr(feature = "std" , derive(Serialize, Deserialize))]
+#[derive(PartialEq, Eq, PartialOrd, Ord, Default, Clone, Encode, Decode, Hash, Debug)]
+pub struct TransactionInput{
+	pub outpoint : H256, //reference to a future UTXO to be spent
+	pub sigscript : H512, //proof of transaction owner
+}
+
+#[cfg_attr(feature = "std" , derive(Serialize, Deserialize))]
+#[derive(PartialEq, Eq, PartialOrd, Ord, Default, Clone, Encode, Decode, Hash, Debug)]
+pub struct TransactionOutput{
+	pub value : Value, //value associated with this UTXO
+	pub pubkey : H256, //public key associated with this output. In order to spend this, the owner must provide a proof by hashing the whole 'Transaction' and signing it with a corresponding private key
+}
+
+#[cfg_attr(feature = "std" , derive(Serialize, Deserialize))]
+#[derive(PartialEq, Eq, PartialOrd, Ord, Default, Clone, Encode, Decode, Hash, Debug)]
+pub struct Transaction{
+	pub inputs : Vec<TransactionInput>,
+	pub outputs : Vec<TransactionOutput>
+}
+
+// this macro is used to designate what gets stored on the blockchain state
+decl_storage! {
+	trait Store for Module<T: Trait> as Utxo {
+		UtxoStore : map H256 => Option<TransactionOutput>;
+	}
+
+	add_extra_genesis{
+		config(genesis_utxos) : Vec<TransactionOutput>
+	}
+}
+
+// External functions: callable by the end user
+decl_module! {
+	pub struct Module<T: Trait> for enum Call where origin: T::Origin {
+		fn deposit_event() = default;
+
+	}
+}
+
+decl_event! {
+	pub enum Event {
+
+	}
+}
+
+/// Tests for this module
+#[cfg(test)]
+mod tests {
+	use super::*;
+
+	use frame_support::{assert_ok, assert_err, impl_outer_origin, parameter_types, weights::Weight};
+	use sp_runtime::{testing::Header, traits::IdentityLookup, Perbill};
+	use sp_core::testing::{KeyStore, SR25519};
+	use sp_core::traits::KeystoreExt;
+
+	impl_outer_origin! {
+		pub enum Origin for Test {}
+	}
+
+	#[derive(Clone, Eq, PartialEq)]
+	pub struct Test;
+	parameter_types! {
+			pub const BlockHashCount: u64 = 250;
+			pub const MaximumBlockWeight: Weight = 1024;
+			pub const MaximumBlockLength: u32 = 2 * 1024;
+			pub const AvailableBlockRatio: Perbill = Perbill::from_percent(75);
+	}
+	impl system::Trait for Test {
+		type Origin = Origin;
+		type Call = ();
+		type Index = u64;
+		type BlockNumber = u64;
+		type Hash = H256;
+		type Hashing = BlakeTwo256;
+		type AccountId = u64;
+		type Lookup = IdentityLookup<Self::AccountId>;
+		type Header = Header;
+		type Event = ();
+		type BlockHashCount = BlockHashCount;
+		type MaximumBlockWeight = MaximumBlockWeight;
+		type MaximumBlockLength = MaximumBlockLength;
+		type AvailableBlockRatio = AvailableBlockRatio;
+		type Version = ();
+		type ModuleToIndex = ();
+		type AccountData = ();
+		type OnNewAccount = ();
+		type OnKilledAccount = ();
+	}
+	impl Trait for Test {
+		type Event = ();
+	}
+
+	type Utxo = Module<Test>;
+
+}
